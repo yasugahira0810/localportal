0956dac05523e080bdc7bd8a9a28db1454401553
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2016-11-05 22:33:56 +0900

    Delete useless codes.

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -3,39 +3,21 @@ angular.module('myapp', ['ngStorage'])
         function($scope, $localStorage) {
             $scope.$storage = $localStorage;
             $scope.$storage.tasks = $scope.$storage.tasks || [{
-                "body": "do this 1",
-                "done": false
+                "title": "ngStorage",
+                "url": "https://github.com/gsklee/ngStorage",
+                "date": new Date()
             }, {
-                "body": "do this 2",
-                "done": false
-            }, {
-                "body": "do this 3",
-                "done": true
-            }, {
-                "body": "do this 4",
-                "done": false
+                "title": "localportal",
+                "url": "https://github.com/yasugahira0810/localportal",
+                "date": new Date()
             }];
 
             $scope.addNew = function() {
                 $scope.$storage.tasks.push({
-                    "body": $scope.newTaskBody,
-                    "done": false
+                    "url": $scope.newTaskBody,
+                    "date": new Date()
                 });
                 $scope.newTaskBody = '';
-            }
-            $scope.getDoneCount = function() {
-                var count = 0;
-                angular.forEach($scope.$storage.tasks, function(task) {
-                    count += task.done ? 1 : 0;
-                });
-                return count;
-            }
-            $scope.deleteDone = function() {
-                var oldTasks = $scope.$storage.tasks;
-                $scope.$storage.tasks = [];
-                angular.forEach(oldTasks, function(task) {
-                    if (!task.done) $scope.$storage.tasks.push(task);
-                });
             }
         }
     ]);
