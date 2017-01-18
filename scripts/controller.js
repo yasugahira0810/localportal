00f2aa965c94252e7c1605a08612f1c65bf71533
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2017-01-18 18:31:11 +0900

    Implement transering variables

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,36 +1,13 @@
-localportal.controller('HomeController', ['$scope', '$localStorage', '$log', '$filter', '$location',
-        function($scope, $localStorage, $log, $filter, $location) {
-            $scope.$storage = $localStorage;
-            $scope.$storage.links = $scope.$storage.links || [{
-                "title": "Google",
-                "url": "https://www.google.co.jp",
-                "tag": "portal",
-                "date": new Date(),
-                "count": 0
-            }, {
-                "title": "ngStorage",
-                "url": "https://github.com/gsklee/ngStorage",
-                "tag": "github,angularjs",
-                "date": new Date(),
-                "count": 0
-            }, {
-                "title": "localportal",
-                "url": "https://github.com/yasugahira0810/localportal",
-                "tag": "github,angularjs,original",
-                "date": new Date(),
-                "count": 0
-            }];
+localportal.controller('HomeController', ['$scope', '$localStorage', '$log', '$filter', '$location', 'storageService',
+        function($scope, $localStorage, $log, $filter, $location, storageService) {
+            $scope.$storage = storageService.$storage;
+            /*$scope.link_limit = 10; // Limit of link number per page
+            $scope.link_start = 0;
+            $scope.page_num = Math.floor($scope.$storage.links.length / $scope.limit_link) + 1;
 
-            $scope.sort = function(exp, reverse) {
-                    $scope.$storage.links = $filter('orderBy')($scope.$storage.links, exp, reverse);
-                }
-                /*$scope.link_limit = 10; // Limit of link number per page
-                $scope.link_start = 0;
-                $scope.page_num = Math.floor($scope.$storage.links.length / $scope.limit_link) + 1;
-
-                $scope.pager = function(page) {
-                    $scope.link_start = $scope.link_limit * page;
-                };*/
+            $scope.pager = function(page) {
+                $scope.link_start = $scope.link_limit * page;
+            };*/
             $scope.pageLimit = 10; // リストの表示件数
             $scope.limitBegin = 0; // リストの表示開始位置
 
@@ -49,13 +26,18 @@ localportal.controller('HomeController', ['$scope', '$localStorage', '$log', '$f
                 return array;
             };
 
+            $scope.sort = function(exp, reverse) {
+                $scope.$storage.links = $filter('orderBy')($scope.$storage.links, exp, reverse);
+            }
+
         }
     ])
     .service('linkService', LinkService)
     .filter('customFilter', CustomFilter);
 
-localportal.controller('InsertController', ['$scope', '$localStorage', '$location',
-    function($scope, $localStorage, $location) {
+localportal.controller('InsertController', ['$scope', '$localStorage', '$location', 'storageService',
+    function($scope, $localStorage, $location, storageService) {
+        $scope.$storage = storageService.$storage;
         $scope.addNew = function() {
             $scope.$storage.links.push({
                 "title": $scope.newLinkTitle,
@@ -69,8 +51,9 @@ localportal.controller('InsertController', ['$scope', '$localStorage', '$locatio
     }
 ]);
 
-localportal.controller('ImportController', ['$scope', '$localStorage', '$location',
-    function($scope, $localStorage, $location) {
+localportal.controller('ImportController', ['$scope', '$localStorage', '$location', 'storageService',
+    function($scope, $localStorage, $location, storageService) {
+        $scope.$storage = storageService.$storage;
         $scope.bulkImport = function() {
             importJson = angular.fromJson($scope.importStr);
             for (i = 0; i < importJson.length; i++) {
@@ -80,6 +63,8 @@ localportal.controller('ImportController', ['$scope', '$localStorage', '$locatio
     }
 ]);
 
-localportal.controller('DisplayController', ['$scope', '$localStorage', '$location',
-    function($scope, $localStorage, $location) {}
+localportal.controller('DisplayController', ['$scope', '$localStorage', '$location', 'storageService',
+    function($scope, $localStorage, $location, storageService) {
+        $scope.$storage = storageService.$storage;
+    }
 ]);
