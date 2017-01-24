58abf467d410d98286d62dd400df835bfefa8a93
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2017-01-25 00:31:53 +0900

    Delete useless templates

--- scripts/route.js
+++ scripts/route.js
@@ -2,22 +2,22 @@ localportal.config(function($routeProvider) {
     $routeProvider
 
     .when('/', {
-        templateUrl: 'pages/home.html',
+        templateUrl: 'home.html',
         controller: 'HomeController'
     })
 
     .when('/insert', {
-        templateUrl: 'pages/insert.html',
+        templateUrl: 'insert.html',
         controller: 'InsertController'
     })
 
     .when('/import', {
-        templateUrl: 'pages/import.html',
+        templateUrl: 'import.html',
         controller: 'ImportController'
     })
 
     .when('/display', {
-        templateUrl: 'pages/display.html',
+        templateUrl: 'display.html',
         controller: 'DisplayController'
     });
 })
